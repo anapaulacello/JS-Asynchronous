9f1835acedbf55136d8846a11705a3e3bff82619
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-10-13 01:23:25 +0200

    2.3

--- iteration1.js
+++ iteration1.js
@@ -12,9 +12,26 @@ const getAllData=async()=>{
 getAllData();
 
 //TODO ultimos ejercicios de la iteracion1
+let newDivDos=document.querySelector('.ejercicioDosUno');
+let nameInput=document.querySelector('.ejercicioDosUno__input');
+
+let getData=async()=>{
+let result=await fetch(`https://api.nationalize.io?name=${nameInput.value}`);
+let resultToJson=await result.json();
+let nameDataHTML=`el nombre <p>${resultToJson.name}</p>
+    tiene un <p>${resultToJson.country[0].probability}</p> porciento de ser 
+    <p>${resultToJson.country[0].country_id}</p> y un <p>${resultToJson.country[1].probability}</p> porciento de ser 
+    <p>${resultToJson.country[1].country_id}</p>`
+
+           newDivDos.innerHTML=nameDataHTML
+}
+
+document.querySelector('.ejercicioDosUno__btn').addEventListener('click',getData)
+
 
 /////////S
 //iteration2
+//TODO ejercicio 1 de esta iteration
 const newRickAndMorty__container=document.querySelector('.RickAndMorty__container');
 const getAllDataRickAndMorty= async()=>{
     const result=await fetch(`https://rickandmortyapi.com/api/character`);
@@ -37,3 +54,17 @@ const getAllDataRickAndMorty= async()=>{
     newRickAndMorty__container.innerHTML=charactersHTML;
 }
 document.querySelector('.rAndm__btn').addEventListener('click',getAllDataRickAndMorty);
+
+/* 2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando 
+async-await.
+const runTimeOut = () => {
+    const promise = new Promise((resolve) => {
+        setTimeout(function () {
+            resolve();
+        }, 2000);
+    })
+
+    promise.then(() => {console.log('Time out!')})
+};
+
+runTimeOut(); */
